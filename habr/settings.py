a8373e54621be73ef191c38adaf4cd8b83a93e68
Denis <pda-gb>
2021-03-29 03:12:47 +0300

    fix: добавил функционал для ветки мастер напрямую.
    Папка json в корне проекта:
    - secret.sample.json (содержит пример заполнения файла secret.json)
    
    Файл secret.json должен содержаться только в локально в этой же папке. Для исключения попадания в гитхаб, занесён в .gitignore. После добавления данной фичи, файл secret.json создать ОБЯЗАТЕЛЬНО ! ! !
    
    Пример содержания файла json/secret.json (можно просто скопировать) :
    
    {
      "SECRET_KEY": "-f%dw@@c%2soul+hxtoh0en!$-j_h(e0e(ockcbi@dqcsbt=(7",
      "SU_name": "django",
      "SU_email": "django@geekshop",
      "SU_password": "geekbrains",
      "SU_age": "1987-12-31",
      "SOCIAL_AUTH_VK_OAUTH2_KEY": "",
      "SOCIAL_AUTH_VK_OAUTH2_SECRET": "",
      "SOCIAL_AUTH_GOOGLE_OAUTH2_KEY": "",
      "SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET": ""
    }

--- habr/settings.py
+++ habr/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import json
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-f%dw@@c%2soul+hxtoh0en!$-j_h(e0e(ockcbi@dqcsbt=(7'
+SECRET_KEY = ''
+with open(os.path.join(BASE_DIR, 'json/secret.json'), 'r') as secret_file:
+    secret_value = json.load(secret_file)
+SECRET_KEY = secret_value["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
