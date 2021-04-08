ee6e84aecb08f1de7afe997fb7bbeb78a6dde109
Denis <pda-gb>
2021-04-08 04:43:27 +0300

    fix

--- apps/articles/views.py
+++ apps/articles/views.py
@@ -16,6 +16,19 @@ def main_page(request):
     return render(request, 'articles/articles.html', page_data)
 
 
+def hub(request, pk=None):
+    if pk == 1:
+        hub_articles = Article.get_articles()
+    else:
+        hub_articles = Article.get_by_hub(pk)
+    hubs_menu = Hub.get_all_hubs()
+    page_data = {
+        'articles': hub_articles,
+        'hubs_menu': hubs_menu
+    }
+    return render(request, 'articles/articles.html', page_data)
+
+
 def article(request, pk=None):
     current_article = Article.get_article(pk)
     hubs_menu = Hub.get_all_hubs()
