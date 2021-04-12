ca9786bea98766edf25d5b051f982f8e170d38cb
Denis <pda-gb>
2021-04-12 23:03:18 +0300

    fix: исправление запуска

--- apps/user_articles/urls.py
+++ apps/user_articles/urls.py
@@ -6,6 +6,7 @@ import apps.user_articles.views as user_articles
 app_name = 'user_articles'
 
 urlpatterns = [
+    path('', user_articles.user_articles, name='user_articles'),
     path('publications/', user_articles.publications, name='publications'),
     path('user_articles/', user_articles.user_articles, name='my_articles'),
     path('draft', user_articles.draft, name='draft'),
--- apps/user_articles/views.py
+++ apps/user_articles/views.py
@@ -6,31 +6,30 @@ def user_articles(request):
     функция отвечает за Мои статьи
     """
     title = 'Мои статьи'
-    
 
     page_data = {
         'title': title,
     }
     return render(request, 'user_articles/user_articles.html', page_data)
 
+
 def publications(request):
     """
     функция отвечает за мои публикации
     """
     title = 'Мои публикации'
-    
 
     page_data = {
         'title': title,
     }
     return render(request, 'user_articles/publications.html', page_data)
 
+
 def draft(request):
     """
     функция отвечает за Черновик
     """
     title = 'Черновик'
-    
 
     page_data = {
         'title': title,
