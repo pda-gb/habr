0e963fbacc7b6e64b5c7533aee46572a4c285bcd
Denis <pda-gb>
2021-04-14 20:51:11 +0300

    слияние урлов

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -5,8 +5,13 @@ import apps.account.views as account
 app_name = "account"
 
 urlpatterns = [
+    path('', account.user_articles, name='user_articles'),
     path("add_article/", account.add_article, name="add_article"),
     path("draft_article/<int:pk>", account.draft_article, name="draft_article"),
     path("del_article/<int:pk>", account.del_article, name="del_article"),
     path("edit_profile/", account.edit_profile, name="edit_profile"),
+    path('publications/', account.publications, name='publications'),
+    path('user_articles/', account.user_articles, name='user_articles'),
+    path('draft', account.draft, name='draft'),
+
 ]
