7aa0018e544726cc26271f157e44caf635e1e9f5
Denis <pda-gb>
2021-04-19 21:35:18 +0300

    fix fill_db

--- apps/articles/management/commands/fill_db.py
+++ apps/articles/management/commands/fill_db.py
@@ -7,14 +7,14 @@ class Command(BaseCommand):
 
     help = 'Наполняем базу данных'
 
-    def add_arguments(self, parser):
-        parser.add_argument('num_users', type=int, help='Количество создаваемых пользователей')
-
-        parser.add_argument('num_articles', type=int, help='Количество создаваемых статей', )
+    # def add_arguments(self, parser):
+    #     parser.add_argument('num_users', type=int, help='Количество создаваемых пользователей')
+    #
+    #     parser.add_argument('num_articles', type=int, help='Количество создаваемых статей', )
 
     def handle(self, *args, **options):
-        num_users = options['num_users']
-        num_articles = options['num_articles']
+        # num_users = options['num_users']
+        # num_articles = options['num_articles']
 
-        os.system(f'python manage.py create_user {num_users}')
-        os.system(f'python manage.py create_article {num_articles}')
+        os.system('python manage.py create_user')
+        os.system('python manage.py create_article')
