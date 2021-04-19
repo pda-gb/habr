312cf24045dfdffbe3404b90831fc0d61eb1ee1c
Denis <pda-gb>
2021-04-20 01:49:40 +0300

    добавление редактора в "редактирование статьи"

--- apps/account/forms.py
+++ apps/account/forms.py
@@ -82,6 +82,8 @@ class ChangePasswordForm(forms.Form):
 
 
 class ArticleEditForm(forms.ModelForm):
+    body = CharField(widget=CKEditorUploadingWidget(config_name="for_user"))
+
     class Meta:
         model = Article
         fields = (
@@ -92,8 +94,19 @@ class ArticleEditForm(forms.ModelForm):
             "link_to_original"
         )
 
-    def __init__(self, *args, **kwargs):
-        super(ArticleEditForm, self).__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs["class"] = "form-control"
-            field.help_text = ""
+# class ArticleEditForm(forms.ModelForm):
+#     class Meta:
+#         model = Article
+#         fields = (
+#             "title",
+#             "hubs",
+#             "body",
+#             "image",
+#             "link_to_original"
+#         )
+#
+#     def __init__(self, *args, **kwargs):
+#         super(ArticleEditForm, self).__init__(*args, **kwargs)
+#         for field_name, field in self.fields.items():
+#             field.widget.attrs["class"] = "form-control"
+#             field.help_text = ""
