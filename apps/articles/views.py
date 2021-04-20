36e69db922a205af626cf90038aa3baced135781
Deni <azureswastika@gmail.com>
2021-04-20 13:29:19 +0300

    Изменил функцию

--- apps/articles/views.py
+++ apps/articles/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from apps.articles.models import Article, ArticleRate
-from apps.authorization.models import HabrUserProfile
 
 
 def main_page(request):
@@ -27,11 +26,11 @@ def hub(request, pk=None):
 def article(request, pk=None):
     last_articles = Article.get_articles()[1]
     current_article = Article.get_article(pk)
-    author_rate = HabrUserProfile.objects.get(id=current_article.author.id).rating
     if request.user.is_authenticated:
-        ArticleRate.create(current_article, request.user)
-    page_data = {"article": current_article, "last_articles": last_articles,
-                 "author_rate": author_rate}
+        rate = ArticleRate.create(current_article, request.user)
+        current_article.user_liked = rate.liked
+        current_article.bookmarked = rate.in_bookmarks
+    page_data = {"article": current_article, "last_articles": last_articles}
     return render(request, "articles/article.html", page_data)
 
 
@@ -41,45 +40,31 @@ def change_article_rate(request):
         article = request.GET.get("article")
         field = request.GET.get("field")
         article_rate = ArticleRate.objects.get(user=user, article=article)
-        author = HabrUserProfile.objects.get(id=Article.get_article(article).author.id)
         if field == "like":
-            if article_rate.liked:
-                article_rate.liked = None
-                author.rating -= 1
-            elif article_rate.liked == None:
-                article_rate.liked = True
-                author.rating += 1
-            else:
-                article_rate.liked = True
-                author.rating += 2
+            article_rate.liked = (
+                True
+                if article_rate.liked is None or article_rate.liked is False
+                else None
+            )
         elif field == "dislike":
-            if article_rate.liked == False:
-                article_rate.liked = None
-                author.rating += 1
-            elif article_rate.liked == None:
-                article_rate.liked = False
-                author.rating -= 1
-            else:
-                article_rate.liked = False
-                author.rating -= 2
+            article_rate.liked = (
+                False
+                if article_rate.liked is None or article_rate.liked is True
+                else None
+            )
         else:
             article_rate.in_bookmarks = not article_rate.in_bookmarks
-        author.save()
-        author_rating = author.rating
         article_rate.save()
         article_objects = ArticleRate.objects.filter(article=article)
-        likes = article_objects.filter(liked=True).count()
-        dislikes = article_objects.filter(liked=False).count()
-        bookmarks = article_objects.filter(in_bookmarks=True).count()
-        article_rate.article.likes = likes
-        article_rate.article.dislikes = dislikes
-        article_rate.article.bookmarks = bookmarks
+        article_rate.article.likes = article_objects.filter(liked=True).count()
+        article_rate.article.dislikes = article_objects.filter(liked=False).count()
+        article_rate.article.rating = article_rate.article.likes - article_rate.article.dislikes
         article_rate.article.save()
+        article_rate.save(True)
         return JsonResponse(
             {
-                "likes": likes,
-                "dislikes": dislikes,
-                "bookmarks": bookmarks,
-                "author_rating": author_rating,
+                "likes": article_rate.article.likes,
+                "dislikes": article_rate.article.dislikes,
+                "author_rating": article_rate.article.author.habruserprofile.rating
             }
         )
--- apps/authorization/models.py
+++ apps/authorization/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.conf import settings
 
 
 class HabrUser(AbstractUser):
