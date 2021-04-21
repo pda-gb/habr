6cabcfd1c61e6c1ba2b934424b81d92d73ff64ae
daniil1 <rzrka1998@gamil.com>
2021-04-21 05:25:19 +0300

    доработка комментов

--- apps/comments/templatetags/comments_tree.py
+++ apps/comments/templatetags/comments_tree.py
@@ -44,29 +44,4 @@ def comments_filter(comments_list):
 
         if comment.get('children'):
             i += comments_filter(comment['children'])
-    print(mark_safe(res.format(i)))
     return mark_safe(res.format(i))
-
-
-'''
- <div class="comment-row">
-                    <h2 class="mb-4">Комментарии</h2>
-                    <hr/>
-                    <div class="m-4">
-                        <div>
-                            <a href="#" class="post-author">
-                                <img src="{% static 'img/avatar.jpg' %}" alt=""/>
-                                Username
-                            </a>
-                            <span>сегодня в 13:00</span>
-                            <div class="comment-rating">
-                                <a href="#"><img src="{% static 'svg/like.svg' %}" alt=""/></a>
-                                <small class="text-muted">-9</small>
-                                <a href="#"><img src="{% static 'svg/dislike(filled).svg' %}" alt=""/></a>
-                            </div>
-                        </div>
-                        <div class="comment-text">
-                            <div>Прочитал, ничего не понял, гугл переводчик тож тупит</div>
-                        </div>
-                    </div>
-'''
