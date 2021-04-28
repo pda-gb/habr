8da382ad76bf578e7543558566a1a4aa8283cc66
Denis <pda-gb>
2021-04-28 12:22:16 +0300

    fix

--- apps/articles/management/commands/create_article.py
+++ apps/articles/management/commands/create_article.py
@@ -14,11 +14,10 @@ class Command(BaseCommand):
         "create_article number_article"
     )
 
-    # def add_arguments(self, parser):
-    #     parser.add_argument('number', type=int, help=u'Количество создаваемых статей')
 
     def add_arguments(self, parser):
-        parser.add_argument("number", type=int, help="Количество создаваемых статей")
+        parser.add_argument("number", type=int,
+                            help="Количество создаваемых статей")
 
     def get_random_query_set_item(self, my_model):
         """
@@ -36,8 +35,8 @@ class Command(BaseCommand):
     def get_list_models(my_model):
         return list(
             my_model.objects.values_list("pk", flat=True)
-            .order_by("?")
-            .distinct()[: randint(1, 5)]
+                .order_by("?")
+                .distinct()[: randint(1, 5)]
         )
 
     def handle(self, *args, **options):
@@ -58,27 +57,36 @@ class Command(BaseCommand):
         # number = 100
         # Создаем хабы
         for hub_item in hubs:
-            if hub_item in Hub.objects.values_list("hub", flat=True).distinct():
-                self.stdout.write(self.style.SUCCESS(f"This hub exist {hub_item}"))
+            if hub_item in Hub.objects.values_list("hub",
+                                                   flat=True).distinct():
+                self.stdout.write(self.style.SUCCESS(f"This hub exist "
+                                                     f"{hub_item}"))
             else:
                 hub_object = Hub(hub=hub_item)
                 hub_object.save()
                 self.stdout.write(
-                    self.style.SUCCESS(f"Successfully created hub {hub_object.hub}")
+                    self.style.SUCCESS(f"Successfully created hub "
+                                       f"{hub_object.hub}")
                 )
         # Создаем теги в количестве, равном половине статей.
         for i in range(number // 2):
             tag_object = Tag(tag=internet.hashtags(quantity=1))
-            if tag_object.tag in Tag.objects.values_list("tag", flat=True).distinct():
+            if tag_object.tag in Tag.objects.values_list("tag",
+                                                         flat=True).distinct():
                 self.stdout.write(
                     self.style.SUCCESS(f"This tag exist {tag_object.tag}")
                 )
             else:
                 tag_object.save()
                 self.stdout.write(
-                    self.style.SUCCESS(f"Successfully created tag {tag_object.tag}")
+                    self.style.SUCCESS(f"Successfully created tag "
+                                       f"{tag_object.tag}")
                 )
-        # Создаем статьи
+
+        ## Создаем статьи
+        # ключевые слова для поиска картинок
+        keyword_img = ['it', 'development', 'design', 'marketing',
+                       'mobile']
         for i in range(number):
             id_hub = randint(1, 4)
             article = Article(
@@ -86,7 +94,9 @@ class Command(BaseCommand):
                 author=self.get_random_query_set_item(HabrUser),
                 body=text.text(quantity=randint(10, 70)),
                 image=internet.stock_image(
-                    width=1920, height=1080, keywords=None, writable=False
+                    width=1920, height=1080,
+                    keywords=[keyword_img[0], keyword_img[id_hub]],
+                    writable=False
                 ),
                 link_to_original=internet.home_page(tld_type=None),
                 draft=False,
@@ -96,7 +106,8 @@ class Command(BaseCommand):
             article.save()
             self.stdout.write(
                 self.style.SUCCESS(
-                    f"Successfully created article by the author {article.author}"
+                    f"Successfully created article by the author "
+                    f"{article.author}"
                 )
             )
 
@@ -109,7 +120,8 @@ class Command(BaseCommand):
             #         )
             #     )
 
-            # Добавляем к статье теги, выбранные случайным образом в количестве от 1 до 5
+            # Добавляем к статье теги, выбранные случайным образом в
+            # количестве от 1 до 5
             article.tags.add(*self.get_list_models(Tag))
             for tag in article.tags.all():
                 self.stdout.write(
