1aeb900e9804fe84861cd6a70fabeff73b9a7410
daniil1 <rzrka1998@gamil.com>
2021-04-28 13:21:21 +0300

    добавил пагинацию исправил лупу

--- static/js/search-articles.js
+++ static/js/search-articles.js
@@ -1,7 +1,9 @@
 $(document).ready(function (){
 	$('#main-magnifier').click(function () {
 		$('.form-search').fadeIn();
-        $('#main-magnifier').fadeOut();
+		$('#main-magnifier').css({
+			'display':'none',
+		})
 		return false;
 	})
 })
