153ff61772b3369552f63984ab4f597da35c0db8
daniil1 <rzrka1998@gamil.com>
2021-05-01 15:50:44 +0300

    добавил иконку сортировки и меню сортировки

---
+++ static/js/sorted-articles.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.sorted-menu').click(function () {
+        $('#form-sorted').css({
+            'display': 'block',
+        });
+    })
+    $(document).click(function(e) {
+        if (!$(e.target).is('.sorted-icon')
+        && !$(e.target).is('.sorted-input')
+        && !$(e.target).is('.sorted-list')
+        && !$(e.target).is('.sorted-label')
+        && !$(e.target).is('.sorted-elem')) {
+            $('#form-sorted').css({
+                'display': 'none',
+            });
+        }
+    })
+})
