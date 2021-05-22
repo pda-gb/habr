f457070c3d465770e69cf34680feb5966fc11571
kostitsyn <kostitsin.a@mail.ru>
2021-05-22 20:38:28 +0400

    Добавил нужные ссылки

--- habr/urls.py
+++ habr/urls.py
@@ -21,3 +21,7 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
+# http://127.0.0.1:8000/complete/vk-oauth2/
+# https://www.sputnik-seven.xyz/complete/vk-oauth2/
+# http://localhost:8000/complete/vk-oauth2/
+# https://coderoad.ru/54375834/%D0%97%D0%B0%D0%BF%D1%83%D1%81%D1%82%D0%B8%D1%82%D0%B5-%D0%BD%D0%B5%D1%81%D0%BA%D0%BE%D0%BB%D1%8C%D0%BA%D0%BE-%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%BE%D0%B2-django-%D1%81-nginx-%D0%B8-gunicorn
